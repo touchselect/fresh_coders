2165f5330208cb8d98880c55bd7c25e8bfe3d715
touchselect <kir191474@gmail.com>
2023-06-13 16:40:27 +0000

    save

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,3 +1,22 @@
 class Public::CommentsController < ApplicationController
 
+  def create
+    post = Post.find(params[:post_id])
+    comment = current_user.comments.new(comment_params)
+    comment.post_id = post.id
+    comment.save
+    render :comment
+  end
+  
+  def destroy
+    Comment.find(params[:id]).destroy
+    render :comment
+  end
+  
+  private
+  
+  def comment_params
+    params.require(:comment).permit(:comment)
+  end
+
 end
