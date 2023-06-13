ec24c839345221aedaf3c95990701dcd09318d30
touchselect <kir191474@gmail.com>
2023-06-13 12:26:18 +0000

    add user_prof

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,3 +1,24 @@
 class Public::UsersController < ApplicationController
   
+  def show
+    @user = User.find(params[:id])
+    @posts = @user.posts
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :user_image)
+  end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,15 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :comments, dependent: :destroy
+  
+  has_one_attached :user_image
+  
+  def get_user_image(width, height)
+    unless user_image.attached?
+      file_path = Rails.root.join('app/assets/images/sample_user.jpg')
+      user_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    user_image.variant(resize_to_limit: [width, height]).processed
+  end
+  
 end
