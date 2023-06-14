399ac6b22fe698ebfc7afe3548a5817aefe6cc0c
touchselect <kir191474@gmail.com>
2023-06-14 16:47:02 +0000

    save

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,14 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   belongs_to :category, optional: true
   
+  scope :search, -> (search_param = nil){
+    return if search_param.blank?
+    joins("INNER JOIN action_text_rich_texts ON
+          action_text_rich_texts.record_id = posts.id AND
+          action_text_rich_texts.record_type = 'Post'")
+    .where("action_text_rich_texts.body LIKE ? OR posts.title LIKE ? ", "%#{search_param}%", "%#{search_param}%")
+  }
+  
   def get_post_image(width, height)
     if post_image.attached?
       post_image.variant(resize_to_limit: [width, height]).processed
